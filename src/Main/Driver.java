0174c6e72516ce91665698d771b4d2d8421027c4
201303524 <peterthorsen.dk@gmail.com>
2016-09-28 21:06:37 +0200

    initial commit

---
+++ src/Main/Driver.java
@@ -0,0 +1,18 @@
+package Main;
+
+/**
+ * Created by Peter on 12/09/16.
+ */
+public class Driver {
+
+  private String url;
+
+  public static void main(String[] args) {
+    new Driver();
+  }
+
+  public Driver() {
+    url = "https://www.boardgamegeek.com/xmlapi/collection/user";
+
+  }
+}
