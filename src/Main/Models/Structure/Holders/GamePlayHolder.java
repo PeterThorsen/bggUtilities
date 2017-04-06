22a473932d9fd604af41d4dbb69d578b65e93853
201303524 <peterthorsen.dk@gmail.com>
2017-04-06 08:58:26 +0200

    Removed standard comments

--- src/Main/Models/Structure/Holders/GamePlayHolder.java
+++ src/Main/Models/Structure/Holders/GamePlayHolder.java
@@ -2,9 +2,6 @@ package Main.Models.Structure.Holders;
 
 import Main.Models.Structure.BoardGame;
 
-/**
- * Created by Peter on 09-Nov-16.
- */
 public class GamePlayHolder {
   public final int plays;
   public final BoardGame game;
--- src/Main/Models/Structure/Holders/NodeHolder.java
+++ src/Main/Models/Structure/Holders/NodeHolder.java
@@ -2,9 +2,6 @@ package Main.Models.Structure.Holders;
 
 import org.w3c.dom.Node;
 
-/**
- * Created by Peter on 06-Dec-16.
- */
 public class NodeHolder {
   public final Node statsNode;
   public final Node numPlaysNode;
