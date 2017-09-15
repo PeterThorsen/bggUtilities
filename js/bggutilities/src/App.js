5666b287f111bbd3757231c6271871b5eaaa3feb
201303524 <peterthorsen.dk@gmail.com>
2017-09-15 09:06:50 +0200

    Fixed react install

--- js/bggutilities/src/App.js
+++ js/bggutilities/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import {$,jQuery} from 'jquery';
 
 class App extends Component {
   render() {
+
+      $.getJSON('localhost:8080', function(data) {
+          console.log(data);
+      });
     return (
       <div className="App">
         Hi
