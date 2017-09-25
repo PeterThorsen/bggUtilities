608a3bacd330f9c46384ce157c87ac5d3df73352
201303524 <peterthorsen.dk@gmail.com>
2017-09-25 09:41:02 +0200

    Using css instead of inline

--- js/bggutilities/src/util/LoadingScreen.js
+++ js/bggutilities/src/util/LoadingScreen.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
+import './LoadingScreen.css';
 
 class LoadingScreen extends Component {
 
     render() {
-        return <div style={{
-            display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '150px'
-        }}>
+        return <div className={"loading-screen"}>
             <CircularProgress/><p>Currently loading data from BoardGameGeek servers. This may take up to 30 seconds.</p>
         </div>
     }
