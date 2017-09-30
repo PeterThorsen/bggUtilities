0b6144e46c40464f5f3ba46b62d4038043b60fd8
201303524 <peterthorsen.dk@gmail.com>
2017-09-30 22:15:54 +0200

    Decided on recharts. Now for implementing throughout app

--- js/bggutilities/src/StatisticsView.js
+++ js/bggutilities/src/StatisticsView.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import {BarChart, PieChart} from "react-d3-basic";
-import {Hint, RadialChart} from 'react-vis';
-import '../node_modules/react-vis/dist/style.css';
-import {VictoryBar, VictoryChart, VictoryAxis, VictoryTheme} from 'victory';
+import {pieColors} from "./util/graphs/GraphUtil";
+//import {BarChart, PieChart} from "react-d3-basic";
+//import {Hint, RadialChart} from 'react-vis';
+//import '../node_modules/react-vis/dist/style.css';
+//import {VictoryBar, VictoryChart, VictoryAxis, VictoryTheme} from 'victory';
+import {Cell, Pie, PieChart} from "recharts";
 
 class StatisticsView extends Component {
 
@@ -26,109 +28,144 @@ class StatisticsView extends Component {
         return recharts;
     }
 
-    getOldCharts() {
+    /*
+        getOldCharts() {
 
-        let generalChartData = [
-            {
-                name: "Charlotte",
-                plays: 339
-            },
-            {
-                name: "Martin",
-                plays: 172
-            },
-            {
-                name: "Marian",
-                plays: 63
-            },
-        ];
-        let chartSeries = [
-            {
-                "field": 'plays',
-                "name": "# Plays"
+            let generalChartData = [
+                {
+                    name: "Charlotte",
+                    plays: 339
+                },
+                {
+                    name: "Martin",
+                    plays: 172
+                },
+                {
+                    name: "Marian",
+                    plays: 63
+                },
+            ];
+            let chartSeries = [
+                {
+                    "field": 'plays',
+                    "name": "# Plays"
+                }
+            ];
+            let chartSeries2 = [
+                {
+                    "field": 'Charlotte',
+                    "name": "Charlotte"
+                },
+                {
+                    "field": 'Martin',
+                    "name": "Martin"
+                },
+                {
+                    "field": 'Marian',
+                    "name": "Marian"
+                },
+            ];
+            let x = (dataPoint) => {
+                return dataPoint.name;
+            };
+
+            let value = (dataPoint) => {
+                return dataPoint.plays;
             }
-        ];
-        let chartSeries2 = [
-            {
-                "field": 'Charlotte',
-                "name": "Charlotte"
-            },
-            {
-                "field": 'Martin',
-                "name": "Martin"
-            },
-            {
-                "field": 'Marian',
-                "name": "Marian"
-            },
-        ];
-        let x = (dataPoint) => {
-            return dataPoint.name;
-        };
 
-        let value = (dataPoint) => {
-            return dataPoint.plays;
+            let xScale = 'ordinal',
+                xLabel = "Names",
+                yLabel = "Plays",
+                yTicks = [10];
+
+            return <div>
+                <BarChart
+                    title={"Plays by players"}
+                    data={generalChartData}
+                    width={600}
+                    height={350}
+                    chartSeries={chartSeries}
+                    x={x}
+                    xLabel={xLabel}
+                    xScale={xScale}
+                    yTicks={yTicks}
+                    yLabel={yLabel}
+                />
+                <div>DIVIDER</div>
+                <PieChart
+                    data={generalChartData}
+                    width={600}
+                    height={350}
+                    chartSeries={chartSeries2}
+                    value={value}
+                    name={x}
+                />
+            </div>
         }
 
-        let xScale = 'ordinal',
-            xLabel = "Names",
-            yLabel = "Plays",
-            yTicks = [10];
+        getVisCharts() {
 
-        return <div>
-            <BarChart
-                title={"Plays by players"}
-                data={generalChartData}
-                width={600}
-                height={350}
-                chartSeries={chartSeries}
-                x={x}
-                xLabel={xLabel}
-                xScale={xScale}
-                yTicks={yTicks}
-                yLabel={yLabel}
-            />
-            <div>DIVIDER</div>
-            <PieChart
-                data={generalChartData}
-                width={600}
-                height={350}
-                chartSeries={chartSeries2}
-                value={value}
-                name={x}
-            />
-        </div>
-    }
+            let myData = [
+                {
+                    label: "Charlotte",
+                    angle: 339,
+                },
+                {
+                    label: "Martin",
+                    angle: 172
+                },
+                {
+                    label: "Marian",
+                    angle: 63
+                },
+            ];
+            return <RadialChart
+                data={myData}
+                innerRadius={100}
+                radius={140}
+                onValueMouseOver={v => this.setState({radialMouseOver: v})}
+                onSeriesMouseOut={v => this.setState({radialMouseOver: false})}
+                width={300}
+                height={300}>
+                {this.state.radialMouseOver && <Hint value={this.state.radialMouseOver}/>}
+            </RadialChart>
+        }
 
-    getVisCharts() {
+        getVictoryCharts() {
+            let data = [
+                {
+                    name: "Charlotte",
+                    plays: 339
+                },
+                {
+                    name: "Martin",
+                    plays: 172
+                },
+                {
+                    name: "Marian",
+                    plays: 63
+                },
+            ];
 
-        let myData = [
-            {
-                label: "Charlotte",
-                angle: 339,
-            },
-            {
-                label: "Martin",
-                angle: 172
-            },
-            {
-                label: "Marian",
-                angle: 63
-            },
-        ];
-        return <RadialChart
-            data={myData}
-            innerRadius={100}
-            radius={140}
-            onValueMouseOver={v => this.setState({radialMouseOver: v})}
-            onSeriesMouseOut={v => this.setState({radialMouseOver: false})}
-            width={300}
-            height={300}>
-            {this.state.radialMouseOver && <Hint value={this.state.radialMouseOver}/>}
-        </RadialChart>
-    }
+            return <VictoryChart
+                domainPadding={40}
+            >
+                <VictoryAxis
+                    tickValues={[1, 2, 3]}
+                    tickFormat={["Charlotte", "Martin", "Marian"]}
+                />
+                <VictoryAxis
+                    dependentAxis
+                />
+                <VictoryBar
+                    data={data}
+                    x="name"
+                    y="plays"
+                />
+            </VictoryChart>
+        }*/
 
-    getVictoryCharts() {
+    getRecharts() {
         let data = [
             {
                 name: "Charlotte",
@@ -143,27 +180,23 @@ class StatisticsView extends Component {
                 plays: 63
             },
         ];
+        const COLORS = pieColors;
 
-        return <VictoryChart
-            domainPadding={40}
-        >
-            <VictoryAxis
-                tickValues={[1, 2, 3]}
-                tickFormat={["Charlotte", "Martin", "Marian"]}
-            />
-            <VictoryAxis
-                dependentAxis
-            />
-            <VictoryBar
+        return <PieChart width={800} height={400}>
+            <Pie
                 data={data}
-                x="name"
-                y="plays"
-            />
-        </VictoryChart>
-    }
-
-    getRecharts() {
-        return <div>temp</div>
+                dataKey={"plays"}
+                innerRadius={60}
+                outerRadius={80}
+                fill="#8884d8"
+                labelLine={false}
+                label={(dataPoint) => {return dataPoint.name}}
+            >
+                {
+                    data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+                }
+            </Pie>
+        </PieChart>
     }
 }
 
---
+++ js/bggutilities/src/util/graphs/GraphUtil.js
@@ -0,0 +1,1 @@
+export const pieColors = ['#FF8042', '#0088FE', '#00C49F', '#FFBB28'];
