253bf7634c96aa8eb01245ffaa6dabeb0a525634
201303524 <peterthorsen.dk@gmail.com>
2017-10-02 07:19:09 +0200

    Added some graphs. Only fitting one is inside Game.js

--- js/bggutilities/src/Game.js
+++ js/bggutilities/src/Game.js
@@ -14,6 +14,8 @@ import {
     TableRowColumn,
 } from 'material-ui/Table';
 import {Tabs, Tab} from 'material-ui/Tabs';
+import {pieColors} from "./util/graphs/GraphUtil";
+import {PieChart, Pie, Cell, Tooltip, Label} from "recharts";
 
 class Game extends Component {
 
@@ -23,89 +25,26 @@ class Game extends Component {
             plays: undefined,
             loading: true,
             game: undefined
-        }
-
-        var request = new XMLHttpRequest();
-        request.timeout = 60000;
-        request.open('GET', 'http://localhost:8080/getGame?id=' + props.match.params.gameId, true);
-        request.send(null);
-        request.onreadystatechange = function () {
-            if (request.readyState === 4 && request.status === 200) {
-                var type = request.getResponseHeader('Content-Type');
-                if (type.indexOf("text") !== 1) {
-                    let result = request.responseText;
-                    if (result === "") {
-                        this.setState({loading: false});
-                    }
-                    else {
-                        let jsonObj = JSON.parse(result);
-                        this.setState({game: jsonObj, loading: false});
-                    }
-                }
-            }
-        }.bind(this);
+        };
+        this.downloadGame();
     }
 
     render() {
         if (this.state.loading) return <LoadingScreen/>;
         if (!this.state.loading && !this.state.game) return <Redirect to={"/games"}/>;
-        let game = this.state.game;
-        let minPlayers = game.minPlayers;
-        let maxPlayers = game.maxPlayers;
-        let minPlaytime = game.minPlaytime;
-        let maxPlaytime = game.maxPlaytime;
-
         if (this.state.plays === undefined) {
-            var request = new XMLHttpRequest();
-            request.timeout = 60000;
-            request.open('GET', 'http://localhost:8080/getPlays?id=' + game.id, true);
-            request.send(null);
-            request.onreadystatechange = function () {
-                if (request.readyState === 4 && request.status === 200) {
-                    var type = request.getResponseHeader('Content-Type');
-                    if (type.indexOf("text") !== 1) {
-                        let result = request.responseText;
-                        let jsonObj = JSON.parse(result);
-                        this.setState({plays: jsonObj});
-                    }
-                }
-            }.bind(this);
+            this.downloadPlays();
         }
+        let infoBlock = this.buildInfoBlock();
         let plays = this.buildPlays();
         let playerRatings = this.buildPlayerRatings();
-
+        let winnerChart = this.getRecharts();
         return <div className="outer-block">
             <div className="main-width">
-                <img src={game.image} alt={""} width={300}/>
-                <div className="main-standard-description">
-                    <div className="main-description-color" style={{marginRight: 65}}>Name</div>
-                    <div>{game.name}</div>
-                </div>
-                <div className="main-standard-description">
-                    <div className="main-description-color" style={{marginRight: 46}}>Playtime</div>
-                    <div>{minPlaytime + (minPlaytime !== maxPlaytime ?
-                        " - " + maxPlaytime : "") + " minutes"}</div>
-                </div>
-                <div className="main-standard-description">
-                    <div className="main-description-color" style={{marginRight: 32}}>Your rating</div>
-                    <div>{parseFloat(game.personalRating).toFixed(2) + "/10"}</div>
-                </div>
-                <div className="main-standard-description">
-                    <div className="main-description-color" style={{marginRight: 21}}>Public rating</div>
-                    <div>{parseFloat(game.averageRating).toFixed(2) + "/10"}</div>
-                </div>
-                <div className="main-standard-description">
-                    <div className="main-description-color" style={{marginRight: 30}}>Complexity</div>
-                    <div>{parseFloat(game.complexity).toFixed(2) + "/5"}</div>
-                </div>
-                <div className="main-standard-description">
-                    <div className="main-description-color" style={{marginRight: 57}}># Plays</div>
-                    <div>{game.numPlays}</div>
-                </div>
-                <div className="main-standard-description">
-                    <div className="main-description-color" style={{marginRight: 55}}>Players</div>
-                    <BestWithBlock minPlayers={minPlayers} maxPlayers={maxPlayers}
-                                   bestWith={game.bestWith} recommendedWith={game.recommendedWith}/>
+                <img src={this.state.game.image} alt={""} width={300}/>
+                <div className="flex-row">
+                    {infoBlock}
+                    {winnerChart}
                 </div>
                 <Divider style={{marginTop: 10, marginBottom: 10}}/>
             </div>
@@ -128,16 +67,56 @@ class Game extends Component {
         this.props.history.push("/plays/" + id);
     }
 
+    buildInfoBlock() {
+        let game = this.state.game;
+        let minPlayers = game.minPlayers;
+        let maxPlayers = game.maxPlayers;
+        let minPlaytime = game.minPlaytime;
+        let maxPlaytime = game.maxPlaytime;
+
+        return <div>
+            <div className="main-standard-description">
+                <div className="main-description-color" style={{marginRight: 65}}>Name</div>
+                <div>{game.name}</div>
+            </div>
+            <div className="main-standard-description">
+                <div className="main-description-color" style={{marginRight: 46}}>Playtime</div>
+                <div>{minPlaytime + (minPlaytime !== maxPlaytime ?
+                    " - " + maxPlaytime : "") + " minutes"}</div>
+            </div>
+            <div className="main-standard-description">
+                <div className="main-description-color" style={{marginRight: 32}}>Your rating</div>
+                <div>{parseFloat(game.personalRating).toFixed(2) + "/10"}</div>
+            </div>
+            <div className="main-standard-description">
+                <div className="main-description-color" style={{marginRight: 21}}>Public rating</div>
+                <div>{parseFloat(game.averageRating).toFixed(2) + "/10"}</div>
+            </div>
+            <div className="main-standard-description">
+                <div className="main-description-color" style={{marginRight: 30}}>Complexity</div>
+                <div>{parseFloat(game.complexity).toFixed(2) + "/5"}</div>
+            </div>
+            <div className="main-standard-description">
+                <div className="main-description-color" style={{marginRight: 57}}># Plays</div>
+                <div>{game.numPlays}</div>
+            </div>
+            <div className="main-standard-description">
+                <div className="main-description-color" style={{marginRight: 55}}>Players</div>
+                <BestWithBlock minPlayers={minPlayers} maxPlayers={maxPlayers}
+                               bestWith={game.bestWith} recommendedWith={game.recommendedWith}/>
+            </div>
+        </div>
+    }
+
     buildPlays() {
         if (!this.state.plays) {
             return <LoadingScreen/>;
         }
 
-        let iteration = 0;
         let plays = [];
 
         this.state.plays.forEach(
-            (play) => {
+            (play, iteration) => {
                 let playerNamesOutput = "";
                 let lastPlayerName = "";
                 for (let inx in play.playerNames) {
@@ -153,7 +132,8 @@ class Game extends Component {
                     playerNamesOutput = playerNamesOutput.substring(0, playerNamesOutput.length - 2);
                 }
 
-                plays.push(<TableRow onTouchTap={() => this.goToPlay(play.id)} key={"play-" + iteration} selectable={false}>
+                plays.push(<TableRow onTouchTap={() => this.goToPlay(play.id)} key={"play-" + iteration}
+                                     selectable={false}>
                     <TableRowColumn>{play.date}</TableRowColumn>
                     <TableRowColumn style={{
                         wordWrap: 'break-word',
@@ -161,7 +141,6 @@ class Game extends Component {
                     }}>{playerNamesOutput}</TableRowColumn>
                     <TableRowColumn>{play.noOfPlays}</TableRowColumn>
                 </TableRow>);
-                iteration++;
             }
         );
         return <div className="main-block">
@@ -185,12 +164,11 @@ class Game extends Component {
             return <LoadingScreen/>;
         }
 
-        let iteration = 0;
         let playerRatings = {};
         let playerRatingsArr = [];
 
         this.state.plays.forEach(
-            (play) => {
+            (play, iteration) => {
                 for (let inx in play.playerNames) {
                     let playerName = play.playerNames[inx];
                     if (play.playerRatings.hasOwnProperty(playerName)) {
@@ -210,7 +188,6 @@ class Game extends Component {
                         }
                     }
                 }
-                iteration++;
             }
         );
 
@@ -221,10 +198,10 @@ class Game extends Component {
             }
         }
         playerRatingsNamesToSort.sort();
-        iteration = 0;
         for (let inx in playerRatingsNamesToSort) {
             let playerName = playerRatingsNamesToSort[inx];
-            playerRatingsArr.push(<TableRow onTouchTap={() => this.goToPlayer(playerName)} key={"player-ratings-" + iteration} selectable={false}>
+            playerRatingsArr.push(<TableRow onTouchTap={() => this.goToPlayer(playerName)}
+                                            key={"player-ratings-" + inx} selectable={false}>
                 <TableRowColumn style={{
                     wordWrap: 'break-word',
                     whiteSpace: 'normal'
@@ -232,7 +209,6 @@ class Game extends Component {
                 <TableRowColumn>{playerRatings[playerName].numberOfPlays}</TableRowColumn>
                 <TableRowColumn>{playerRatings[playerName].rating === undefined ? "N/A" : playerRatings[playerName].rating}</TableRowColumn>
             </TableRow>);
-            iteration++;
         }
 
         return <div className="main-block">
@@ -249,6 +225,108 @@ class Game extends Component {
                 </TableBody>
             </Table>
         </div>;
+    }
+
+    getRecharts() {
+
+        if (!this.state.plays) {
+            return <LoadingScreen/>;
+        }
+        let data = [];
+        this.state.plays.forEach(
+            (play) => {
+                let noOfPlays = play.noOfPlays;
+                play.playerNames.forEach(
+                    (player) => {
+                        let found = false;
+
+                        data.forEach(
+                            (dataPoint) => {
+                                if (dataPoint.name === player) {
+                                    dataPoint.plays += noOfPlays;
+                                    found = true;
+                                }
+                            }
+                        );
+
+                        if (!found) {
+                            data.push({name: player, plays: noOfPlays});
+                        }
+                    }
+                );
+            }
+        );
+
+
+        let totalPlays = 0;
+        data.forEach(
+            (element) => {
+                totalPlays += element.plays;
+            }
+        );
+        const COLORS = pieColors;
+
+        return <PieChart width={300} height={200}>
+            <Pie
+                data={data}
+                dataKey={"plays"}
+                cx={150}
+                cy={100}
+                innerRadius={30}
+                outerRadius={50}
+                fill="#8884d8"
+                labelLine={false}
+                label={(dataPoint) => {
+                    return dataPoint.plays > totalPlays / 10 ? dataPoint.name : "";
+                }}
+            >
+                <Label value="Plays" position="center"/>
+                {
+                    data.map((entry, index) => <Cell key={"cell-"+index} fill={COLORS[index % COLORS.length]}/>)
+
+                }
+            </Pie>
+            <Tooltip />
+        </PieChart>
+    }
+
+    downloadGame() {
+        var request = new XMLHttpRequest();
+        request.timeout = 60000;
+        request.open('GET', 'http://localhost:8080/getGame?id=' + this.props.match.params.gameId, true);
+        request.send(null);
+        request.onreadystatechange = function () {
+            if (request.readyState === 4 && request.status === 200) {
+                var type = request.getResponseHeader('Content-Type');
+                if (type.indexOf("text") !== 1) {
+                    let result = request.responseText;
+                    if (result === "") {
+                        this.setState({loading: false});
+                    }
+                    else {
+                        let jsonObj = JSON.parse(result);
+                        this.setState({game: jsonObj, loading: false});
+                    }
+                }
+            }
+        }.bind(this);
+    }
+
+    downloadPlays() {
+        var request = new XMLHttpRequest();
+        request.timeout = 60000;
+        request.open('GET', 'http://localhost:8080/getPlays?id=' + this.state.game.id, true);
+        request.send(null);
+        request.onreadystatechange = function () {
+            if (request.readyState === 4 && request.status === 200) {
+                var type = request.getResponseHeader('Content-Type');
+                if (type.indexOf("text") !== 1) {
+                    let result = request.responseText;
+                    let jsonObj = JSON.parse(result);
+                    this.setState({plays: jsonObj});
+                }
+            }
+        }.bind(this);
     }
 }
 
--- js/bggutilities/src/StatisticsView.js
+++ js/bggutilities/src/StatisticsView.js
@@ -1,171 +1,33 @@
 import React, {Component} from 'react';
 import {pieColors} from "./util/graphs/GraphUtil";
-//import {BarChart, PieChart} from "react-d3-basic";
-//import {Hint, RadialChart} from 'react-vis';
-//import '../node_modules/react-vis/dist/style.css';
-//import {VictoryBar, VictoryChart, VictoryAxis, VictoryTheme} from 'victory';
-import {Cell, Pie, PieChart} from "recharts";
+import LoadingScreen from "./util/LoadingScreen";
+import {
+    Bar, BarChart, Cell, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis,
+    YAxis
+} from "recharts";
 
 class StatisticsView extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            radialMouseOver: false
+            allGames: undefined,
+            allPlayers: undefined,
+            loadingGames: true,
+            loadingPlayers: true
         }
+        this.downloadAllGames();
+        this.downloadAllPlayers();
     }
 
     render() {
-
-        //let d3Charts = this.getOldCharts();
-
-        //let visCharts = this.getVisCharts();
-
-        //let victoryCharts = this.getVictoryCharts();
-
-        let recharts = this.getRecharts();
-
-        return recharts;
+        if (this.state.loadingGames || this.state.loadingPlayers) return <LoadingScreen/>;
+        let complexityChart = this.getComplexityBarChart();
+        let ratingsDifferenceChart = this.getRatingsDifferenceChart();
+        return ratingsDifferenceChart;
     }
 
-    /*
-        getOldCharts() {
-
-            let generalChartData = [
-                {
-                    name: "Charlotte",
-                    plays: 339
-                },
-                {
-                    name: "Martin",
-                    plays: 172
-                },
-                {
-                    name: "Marian",
-                    plays: 63
-                },
-            ];
-            let chartSeries = [
-                {
-                    "field": 'plays',
-                    "name": "# Plays"
-                }
-            ];
-            let chartSeries2 = [
-                {
-                    "field": 'Charlotte',
-                    "name": "Charlotte"
-                },
-                {
-                    "field": 'Martin',
-                    "name": "Martin"
-                },
-                {
-                    "field": 'Marian',
-                    "name": "Marian"
-                },
-            ];
-            let x = (dataPoint) => {
-                return dataPoint.name;
-            };
-
-            let value = (dataPoint) => {
-                return dataPoint.plays;
-            }
-
-            let xScale = 'ordinal',
-                xLabel = "Names",
-                yLabel = "Plays",
-                yTicks = [10];
-
-            return <div>
-                <BarChart
-                    title={"Plays by players"}
-                    data={generalChartData}
-                    width={600}
-                    height={350}
-                    chartSeries={chartSeries}
-                    x={x}
-                    xLabel={xLabel}
-                    xScale={xScale}
-                    yTicks={yTicks}
-                    yLabel={yLabel}
-                />
-                <div>DIVIDER</div>
-                <PieChart
-                    data={generalChartData}
-                    width={600}
-                    height={350}
-                    chartSeries={chartSeries2}
-                    value={value}
-                    name={x}
-                />
-            </div>
-        }
-
-        getVisCharts() {
-
-            let myData = [
-                {
-                    label: "Charlotte",
-                    angle: 339,
-                },
-                {
-                    label: "Martin",
-                    angle: 172
-                },
-                {
-                    label: "Marian",
-                    angle: 63
-                },
-            ];
-            return <RadialChart
-                data={myData}
-                innerRadius={100}
-                radius={140}
-                onValueMouseOver={v => this.setState({radialMouseOver: v})}
-                onSeriesMouseOut={v => this.setState({radialMouseOver: false})}
-                width={300}
-                height={300}>
-                {this.state.radialMouseOver && <Hint value={this.state.radialMouseOver}/>}
-            </RadialChart>
-        }
-
-        getVictoryCharts() {
-            let data = [
-                {
-                    name: "Charlotte",
-                    plays: 339
-                },
-                {
-                    name: "Martin",
-                    plays: 172
-                },
-                {
-                    name: "Marian",
-                    plays: 63
-                },
-            ];
-
-            return <VictoryChart
-                domainPadding={40}
-            >
-                <VictoryAxis
-                    tickValues={[1, 2, 3]}
-                    tickFormat={["Charlotte", "Martin", "Marian"]}
-                />
-                <VictoryAxis
-                    dependentAxis
-                />
-                <VictoryBar
-                    data={data}
-                    x="name"
-                    y="plays"
-                />
-            </VictoryChart>
-        }*/
-
-    getRecharts() {
+    getPieChart() {
         let data = [
             {
                 name: "Charlotte",
@@ -190,13 +52,140 @@ class StatisticsView extends Component {
                 outerRadius={80}
                 fill="#8884d8"
                 labelLine={false}
-                label={(dataPoint) => {return dataPoint.name}}
+                label={(dataPoint) => {
+                    return dataPoint.name
+                }}
             >
                 {
                     data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
                 }
             </Pie>
         </PieChart>
+    }
+
+    getComplexityBarChart() {
+        let data = [
+            {
+                range: "1-2",
+                Games: 0
+            },
+            {
+                range: "2-3",
+                Games: 0
+            },
+            {
+                range: "3-4",
+                Games: 0
+            },
+            {
+                range: "4-5",
+                Games: 0
+            },
+        ];
+
+        this.state.allGames.forEach(
+            (game) => {
+                let complexity = game.complexity;
+                if (complexity < 2) {
+                    data[0].Games++;
+                }
+                else if (complexity < 3) {
+                    data[1].Games++;
+                }
+                else if (complexity < 4) {
+                    data[2].Games++;
+                }
+                else {
+                    data[3].Games++;
+                }
+            }
+        )
+        return <BarChart width={600} height={300} data={data}
+                         margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <XAxis dataKey="range"/>
+            <YAxis/>
+            <Bar dataKey="Games" fill="#8884d8"/>
+            <Legend/>
+        </BarChart>
+    }
+
+    getRatingsDifferenceChart() {
+        let data = [];
+
+        this.state.allGames.forEach(
+            (game) => {
+
+                let playersAverageRatings = 0;
+                let playersHavingPlayedTheGame = 0;
+                this.state.allPlayers.forEach(
+                    (player) => {
+                        let playerRating = player.gameRatingsMap[game.name];
+                        if (playerRating) {
+                            playersAverageRatings += playerRating;
+                            playersHavingPlayedTheGame++;
+                        }
+                    }
+                );
+                playersAverageRatings = playersAverageRatings !== 0 ? playersAverageRatings / playersHavingPlayedTheGame : 0;
+                playersAverageRatings = parseFloat(playersAverageRatings).toFixed(2);
+
+                if (game.averageRating && game.personalRating && playersAverageRatings) {
+                    data.push(
+                        {
+                            name: game.name,
+                            publicRating: parseFloat(game.averageRating),
+                            yourRating: parseFloat(game.personalRating),
+                            friendsAverageRating: parseFloat(playersAverageRatings)
+                        }
+                    )
+                }
+            }
+        );
+
+        return <LineChart width={600} height={250} data={data}
+                          margin={{top: 50, right: 30, left: 20, bottom: 5}}>
+            <XAxis dataKey="name"/>
+            <YAxis domain={[5, 10]}/>
+            <Tooltip/>
+            <Legend/>
+            <Line type="monotone" dataKey="publicRating" stroke={pieColors[0]}/>
+            <Line type="monotone" dataKey="yourRating" stroke={pieColors[1]}/>
+            <Line type="monotone" dataKey="friendsAverageRating" stroke={pieColors[2]}/>
+        </LineChart>
+    }
+
+    downloadAllGames() {
+        var request = new XMLHttpRequest();
+        request.timeout = 60000;
+        request.open('GET', 'http://localhost:8080/getGames?expansions=' + false, true);
+        request.send(null);
+        request.onreadystatechange = function () {
+            if (request.readyState === 4 && request.status === 200) {
+                var type = request.getResponseHeader('Content-Type');
+                if (type.indexOf("text") !== 1) {
+                    let result = request.responseText;
+                    let jsonObj = JSON.parse(result);
+                    this.setState({loadingGames: false, allGames: jsonObj});
+                }
+            }
+        }.bind(this);
+    }
+
+    downloadAllPlayers() {
+        var request = new XMLHttpRequest();
+        request.timeout = 60000;
+        request.open('GET', 'http://localhost:8080/getAllPlayers', true);
+        request.send(null);
+        request.onreadystatechange = function () {
+            if (request.readyState === 4 && request.status === 200) {
+                var type = request.getResponseHeader('Content-Type');
+                if (type.indexOf("text") !== 1) {
+                    let result = request.responseText;
+                    let jsonObj = JSON.parse(result);
+                    this.setState({loadingPlayers: false, allPlayers: jsonObj});
+                }
+            }
+        }.bind(this);
     }
 }
 
