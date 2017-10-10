9297bc723cf993d81fc90cc0df1dbad781829333
201303524 <peterthorsen.dk@gmail.com>
2017-10-10 07:47:20 +0200

    Can now sort tables in PlayersView

--- js/bggutilities/src/PlayersView.js
+++ js/bggutilities/src/PlayersView.js
@@ -10,6 +10,7 @@ import {
     TableRowColumn,
 } from 'material-ui/Table';
 import {withRouter} from "react-router-dom";
+import FlatButton from 'material-ui/FlatButton';
 
 class PlayersView extends Component {
 
@@ -17,7 +18,14 @@ class PlayersView extends Component {
         super(props);
         this.state = {
             found: false,
-            result: undefined,
+            currentName: undefined,
+            players: undefined,
+            playersSortedByName: undefined,
+            playersSortedByPlays: undefined,
+            playersSortedByMinComplexity: undefined,
+            playersSortedByMaxComplexity: undefined,
+            playersSortedByAverageComplexity: undefined,
+            playersSortedByMagicComplexity: undefined
         }
     }
 
@@ -30,7 +38,7 @@ class PlayersView extends Component {
         else {
             let result = [];
             let rowNumber = 0;
-            this.state.result.forEach(
+            this.state.players.forEach(
                 (player) => {
                     let magicComplexity = parseFloat(player.magicComplexity).toFixed(2);
                     result.push(
@@ -38,33 +46,76 @@ class PlayersView extends Component {
                                   onTouchTap={() => this.goToPlayer(player)}>
                             <TableRowColumn style={{width: 80}}>{player.name}</TableRowColumn>
                             <TableRowColumn style={{width: 40}}>{player.totalPlays}</TableRowColumn>
-                            <TableRowColumn style={{width: 50}}>{parseFloat(player.minComplexity).toFixed(2)}</TableRowColumn>
-                            <TableRowColumn style={{width: 50}}>{parseFloat(player.maxComplexity).toFixed(2)}</TableRowColumn>
-                            <TableRowColumn style={{width: 60}}>{parseFloat(player.averageComplexity).toFixed(2)}</TableRowColumn>
-                            <TableRowColumn style={{width: 60}}>{magicComplexity !== '0.00' ? magicComplexity : "N/A"}</TableRowColumn>
+                            <TableRowColumn
+                                style={{width: 50}}>{parseFloat(player.minComplexity).toFixed(2)}</TableRowColumn>
+                            <TableRowColumn
+                                style={{width: 50}}>{parseFloat(player.maxComplexity).toFixed(2)}</TableRowColumn>
+                            <TableRowColumn
+                                style={{width: 60}}>{parseFloat(player.averageComplexity).toFixed(2)}</TableRowColumn>
+                            <TableRowColumn
+                                style={{width: 60}}>{magicComplexity !== '0.00' ? magicComplexity : "N/A"}</TableRowColumn>
                         </TableRow>);
                     rowNumber++;
                 }
             );
-
+            let buttonStyle = {
+                textAlign: 'left',
+                marginLeft: -15
+            };
             mainBlock = <div className="main-block">
                 <Table style={{width: 600}}>
                     <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
                         <TableRow>
-                            <TableHeaderColumn style={{width: 80}}>Name</TableHeaderColumn>
-                            <TableHeaderColumn style={{width: 40}}># Plays</TableHeaderColumn>
-                            <TableHeaderColumn style={{width: 50}}>Min complexity</TableHeaderColumn>
-                            <TableHeaderColumn style={{width: 50}}>Max complexity</TableHeaderColumn>
+                            <TableHeaderColumn style={{width: 80}}>
+                                <FlatButton label="Name" fullWidth={true}
+                                            labelStyle={{textTransform: 'none'}}
+                                            style={buttonStyle}
+                                            hoverColor="none" disableTouchRipple={true}
+                                            onTouchTap={this.sortByName.bind(this)}/>
+                            </TableHeaderColumn>
+                            <TableHeaderColumn style={{width: 40}}>
+                                <FlatButton label="Plays" fullWidth={true}
+                                            labelStyle={{textTransform: 'none'}}
+                                            style={buttonStyle}
+                                            hoverColor="none" disableTouchRipple={true}
+                                            onTouchTap={this.sortByPlays.bind(this)}/>
+                            </TableHeaderColumn>
+                            <TableHeaderColumn style={{width: 50}}>
+                                <FlatButton label="Min" fullWidth={true}
+                                            labelStyle={{textTransform: 'none'}}
+                                            style={buttonStyle}
+                                            hoverColor="none" disableTouchRipple={true}
+                                            onTouchTap={this.sortByMinComplexity.bind(this)}/>
+                            </TableHeaderColumn>
+                            <TableHeaderColumn style={{width: 50}}>
+                                <FlatButton label="Max" fullWidth={true}
+                                            labelStyle={{textTransform: 'none'}}
+                                            style={buttonStyle}
+                                            hoverColor="none" disableTouchRipple={true}
+                                            onTouchTap={this.sortByMaxComplexity.bind(this)}/>
+                            </TableHeaderColumn>
                             <TableHeaderColumn style={{
                                 width: 60,
                                 wordWrap: 'break-word',
                                 whiteSpace: 'normal'
-                            }}>Average complexity</TableHeaderColumn>
+                            }}>
+                                <FlatButton label="Average" fullWidth={true}
+                                            labelStyle={{textTransform: 'none'}}
+                                            style={buttonStyle}
+                                            hoverColor="none" disableTouchRipple={true}
+                                            onTouchTap={this.sortByAverageComplexity.bind(this)}/>
+                            </TableHeaderColumn>
                             <TableHeaderColumn style={{
                                 width: 60,
                                 wordWrap: 'break-word',
                                 whiteSpace: 'normal'
-                            }}>Magic complexity</TableHeaderColumn>
+                            }}>
+                                <FlatButton label="Magic" fullWidth={true}
+                                            labelStyle={{textTransform: 'none'}}
+                                            style={buttonStyle}
+                                            hoverColor="none" disableTouchRipple={true}
+                                            onTouchTap={this.sortByMagicComplexity.bind(this)}/>
+                            </TableHeaderColumn>
                         </TableRow>
                     </TableHeader>
                     <TableBody displayRowCheckbox={false}>
@@ -78,6 +129,10 @@ class PlayersView extends Component {
         </div>
     }
 
+    goToPlayer(player) {
+        this.props.history.push("players/" + player.name);
+    }
+
     getPlayers() {
         var request = new XMLHttpRequest();
         request.timeout = 60000;
@@ -89,14 +144,202 @@ class PlayersView extends Component {
                 if (type.indexOf("text") !== 1) {
                     let result = request.responseText;
                     let jsonObj = JSON.parse(result);
-                    this.setState({found: true, result: jsonObj});
+                    jsonObj.forEach(
+                        (obj) => {
+                            obj.allPlays = undefined;
+                            obj.gameRatingsMap = undefined;
+                            obj.gameToPlaysMap = undefined;
+                            obj.playerNameToPlaysMap = undefined;
+                        }
+                    );
+                    let playersSortedByName = this.buildSortedByName(jsonObj);
+                    let playersSortedByPlays = this.buildSortedByPlays(jsonObj);
+                    let playersSortedByMinComplexity = this.buildSortedByMinComplexity(jsonObj);
+                    let playersSortedByMaxComplexity = this.buildSortedByMaxComplexity(jsonObj);
+                    let playersSortedByAverageComplexity = this.buildSortedByAverageComplexity(jsonObj);
+                    let playersSortedByMagicComplexity = this.buildSortedByMagicComplexity(jsonObj);
+
+                    this.setState({
+                        found: true,
+                        currentName: "name",
+                        players: playersSortedByName,
+                        playersSortedByName: playersSortedByName,
+                        playersSortedByPlays: playersSortedByPlays,
+                        playersSortedByMinComplexity: playersSortedByMinComplexity,
+                        playersSortedByMaxComplexity: playersSortedByMaxComplexity,
+                        playersSortedByAverageComplexity: playersSortedByAverageComplexity,
+                        playersSortedByMagicComplexity: playersSortedByMagicComplexity
+                    });
                 }
             }
         }.bind(this);
     }
 
-    goToPlayer(player) {
-        this.props.history.push("players/" + player.name);
+    buildSortedByName(allPlayers) {
+        let byName = allPlayers.slice();
+
+        // Sorted by server
+        /*byName.sort(
+            (x, y) => {
+                return x.name.localeCompare(y.name);
+            }
+        );*/
+        return byName;
+    }
+
+    buildSortedByPlays(allPlayers) {
+        let byPlays = allPlayers.slice();
+        byPlays.sort(
+            (x, y) => {
+                return y.totalPlays - x.totalPlays;
+
+            }
+        );
+        return byPlays;
+    }
+
+    buildSortedByMinComplexity(allPlayers) {
+        let byComplexity = allPlayers.slice();
+        byComplexity.sort(
+            (x, y) => {
+                return y.minComplexity - x.minComplexity;
+
+            }
+        );
+        return byComplexity;
+
+    }
+
+    buildSortedByMaxComplexity(allPlayers) {
+        let byComplexity = allPlayers.slice();
+        byComplexity.sort(
+            (x, y) => {
+                return y.maxComplexity - x.maxComplexity;
+
+            }
+        );
+        return byComplexity;
+    }
+
+    buildSortedByAverageComplexity(allPlayers) {
+        let byComplexity = allPlayers.slice();
+        byComplexity.sort(
+            (x, y) => {
+                return y.averageComplexity - x.averageComplexity;
+
+            }
+        );
+        return byComplexity;
+    }
+
+    buildSortedByMagicComplexity(allPlayers) {
+        let byComplexity = allPlayers.slice();
+        byComplexity.sort(
+            (x, y) => {
+                return y.magicComplexity - x.magicComplexity;
+
+            }
+        );
+        return byComplexity;
+    }
+
+
+    // Sorting table
+
+    sortByName() {
+        if (this.state.currentName === "name") {
+            let reversed = this.state.playersSortedByName.slice().reverse();
+            this.setState({
+                players: reversed,
+                currentName: "name-reverse"
+            })
+        }
+        else {
+            this.setState({
+                players: this.state.playersSortedByName,
+                currentName: "name"
+            })
+        }
+    }
+
+    sortByPlays() {
+        if (this.state.currentName === "plays") {
+            let reversed = this.state.playersSortedByPlays.slice().reverse();
+            this.setState({
+                players: reversed,
+                currentName: "plays-reverse"
+            })
+        }
+        else {
+            this.setState({
+                players: this.state.playersSortedByPlays,
+                currentName: "plays"
+            })
+        }
+    }
+
+    sortByMinComplexity() {
+        if (this.state.currentName === "min") {
+            let reversed = this.state.playersSortedByMinComplexity.slice().reverse();
+            this.setState({
+                players: reversed,
+                currentName: "min-reverse"
+            })
+        }
+        else {
+            this.setState({
+                players: this.state.playersSortedByMinComplexity,
+                currentName: "min"
+            })
+        }
+    }
+
+    sortByMaxComplexity() {
+        if (this.state.currentName === "max") {
+            let reversed = this.state.playersSortedByMaxComplexity.slice().reverse();
+            this.setState({
+                players: reversed,
+                currentName: "max-reverse"
+            })
+        }
+        else {
+            this.setState({
+                players: this.state.playersSortedByMaxComplexity,
+                currentName: "max"
+            })
+        }
+    }
+
+    sortByAverageComplexity() {
+        if (this.state.currentName === "avg") {
+            let reversed = this.state.playersSortedByAverageComplexity.slice().reverse();
+            this.setState({
+                players: reversed,
+                currentName: "avg-reverse"
+            })
+        }
+        else {
+            this.setState({
+                players: this.state.playersSortedByAverageComplexity,
+                currentName: "avg"
+            })
+        }
+    }
+
+    sortByMagicComplexity() {
+        if (this.state.currentName === "magic") {
+            let reversed = this.state.playersSortedByMagicComplexity.slice().reverse();
+            this.setState({
+                players: reversed,
+                currentName: "magic-reverse"
+            })
+        }
+        else {
+            this.setState({
+                players: this.state.playersSortedByMagicComplexity,
+                currentName: "magic"
+            })
+        }
     }
 }
 
